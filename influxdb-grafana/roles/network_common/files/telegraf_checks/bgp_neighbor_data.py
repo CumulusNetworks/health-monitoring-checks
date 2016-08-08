357f11d1281f2b2275d8c1c48d8f6735c554af8c
Pete Lumbis <plumbis@cumulusnetworks.com>
2016-08-08 18:10:17 -0400

    Changing shebang to python2.7. ujson not supported in python 3 version on CL

--- influxdb-grafana/roles/network_common/files/telegraf_checks/bgp_neighbor_data.py
+++ influxdb-grafana/roles/network_common/files/telegraf_checks/bgp_neighbor_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2.7
 
 # Note: You can't run this in the same folder (network_common/files) as ujson.so.
 # Python will try to load the .so and it will fail since it was built for CL
