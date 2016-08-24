994143f9a7217a4a5aa61b943b0a4f4c2d17a279
rdarbha <rama.h.darbha@gmail.com>
2016-08-24 17:13:12 -0400

    Adding hwenv script

---
+++ influxdb-grafana/roles/network_common/files/telegraf_checks/hwenv_data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#python parser
+import sys
+import json
+import subprocess
+#Parse Args
+if len(sys.argv)!=2:
+    print "ERROR: Need a single argument."
+    print "   Usage: %s interface_state" % sys.argv[0]
+    exit(1)
+#Collect Output
+output=None
+
+output=subprocess.check_output(['/usr/sbin/smonctl -j'],shell=True)
+
+# if sys.argv[1] == "fan":
+#     output=subprocess.check_output(['/usr/bin/netshow interface all -j'],shell=True)
+# elif sys.argv[1] == "memory":
+#     output=subprocess.check_output(['/usr/cumulus/bin/cl-netstat -j'],shell=True)
+# else:
+#     print "   Usage: %s interface_state" % sys.argv[0]
+#     exit(1)
+hostname=subprocess.check_output(['/bin/hostname'],shell=True).replace("\n","")
+#Parse and Display Output
+parsed_output=json.loads(output)
+
+for item in parsed_output:
+    # print 'hwenv_state,host=%s,interface=%s state="%s"' %(hostname,item,parsed_output[item]['linkstate'])
+    # print 'hwenv_state,host=%s,interface=%s speed="%s"' %(hostname,item,parsed_output[item]['speed'])
+    print item;
+exit(0)
