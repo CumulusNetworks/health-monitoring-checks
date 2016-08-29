f366d579d794530cf9172e47f7534563759f4a12
rdarbha <rama.h.darbha@gmail.com>
2016-08-29 18:28:17 -0400

    Adding system environmentals

---
+++ influxdb-grafana/roles/network_common/files/telegraf_checks/sysenv_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#python parser
+import psutil
+import sys
+import json
+import subprocess
+#Parse Args
+# if len(sys.argv)!=2:
+#     print "ERROR: Need a single argument."
+#     print "   Usage: %s interface_state" % sys.argv[0]
+#     exit(1)
+#Collect Output
+output=None
+
+hostname=subprocess.check_output(['/bin/hostname'],shell=True).replace("\n","")
+
+if sys.argv[1] == "cpu":
+    output=psutil.cpu_percent(interval=None, percpu=True)
+    print 'systemenv,host=%s,device=cpu cpu=%s' %(hostname,output)
+elif sys.argv[1] == "memory":
+    output=psutil.virtual_memory()
+    print 'systemenv,host=%s,device=memused percent_used=%s' %(hostname,output.percent)
+elif sys.argv[1] == "disk":
+    output=psutil.disk_usage('/')
+    print 'systemenv,host=%s,device=diskused percent_used=%s' %(hostname,output.percent)
+else:
+    print "   Usage: %s cpu|memory|disk" % sys.argv[0]
+    exit(1)
+
+#Parse and Display Output
+# parsed_output=json.loads(output)
+
+# for item in parsed_output:
+#     print 'systemenv,host=%s,device=%s state="%s"' %(hostname,item['name'],item['state'])
+
+#     if 'input' in item:
+#         print 'hwenv_state,host=%s,device=%s input="%s"' %(hostname,item['name'],item['input'])
+exit(0)
