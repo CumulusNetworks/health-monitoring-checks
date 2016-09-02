c83fe87966316d12b09853cb987cad9a4e901005
rdarbha <rama.h.darbha@gmail.com>
2016-09-02 14:51:24 -0400

    Updating playbook to copy over all monitoring scripts

--- cldemo/roles/monitoring_agent/files/telegraf_checks/interface_data.py
+++ cldemo/roles/monitoring_agent/files/telegraf_checks/interface_data.py
@@ -1,59 +1,41 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/python
 
-# Note: You can't run this in the same folder (network_common/files) as ujson.so.
-# Python will try to load the .so and it will fail since it was built for CL
-
+import json
 import subprocess
-import ujson
-import socket
+from output_module import ExportData
 
-sudo = "/usr/bin/sudo"
-vtysh = "/usr/bin/vtysh"
-netshow = "/usr/bin/netshow"
+"""
+Collects all interface info.
+"""
 
+def collect_data():
+    output=None
+    output=subprocess.check_output(['/usr/cumulus/bin/cl-netstat -j'],shell=True)
+    parsed_output=json.loads(output)
+    #data = ExportData(data_set_name,fixed_tags,data)
+    data = ExportData("iface_info",{})
 
-def run_json_command(command):
-    """
-    Run a command that returns json data. Takes in a list of command arguments
-    :return: JSON string output from command
-    """
-    json_str = ''
-    try:
-        json_str = subprocess.check_output(command)
-    except OSError as e:
-        print("problem executing vtysh %s " % (e))
-        exit(3)
+    for item in parsed_output:
+        #data.add_row({tag_name:tag_value,datapoint_name:datapoint_value}
+        data.add_row({"interface":item,"RX_OK":parsed_output[item]['RX_OK']})
+        data.add_row({"interface":item,"TX_OK":parsed_output[item]['TX_OK']})
+        data.add_row({"interface":item,"RX_DRP":parsed_output[item]['RX_DRP']})
+        data.add_row({"interface":item,"TX_DRP":parsed_output[item]['TX_DRP']})
 
-    return json_str
+    output=subprocess.check_output(['/usr/bin/netshow interface all -j'],shell=True)
+    parsed_output=json.loads(output)
 
+    for item in parsed_output:
+        #data.add_row({tag_name:tag_value,datapoint_name:datapoint_value}
+        data.add_row({"interface":item,"linkstate":parsed_output[item]['linkstate']})
+        data.add_row({"interface":item,"speed":parsed_output[item]['speed']})
 
-def interface_data():
+    #Use this to sanity check the datastructure
+    #data.show_data()
 
-    # Gather output of interfaces
+    #Send the data
+    data.send_data("cli")
 
-    interface_data_output = run_json_command(
-        [sudo, netshow, "interface", "all", "-j"])
+collect_data()
 
-    # if bgp is not configured no output is returned
-    if len(interface_data_output) == 0:
-        print("No interface output. Is the hardware detected properly?")
-        exit(3)
-
-    json_interface_data = ujson.loads(interface_data_output.decode('utf-8'))
-
-    if len(json_interface_data) == 0:
-        print("No interfaces configured. Are any interfaces configured?")
-        exit(3)
-
-    # BGP is configured and peers exist.
-
-    for interface in json_interface_data.keys():
-        # print(interface)
-        print("intstat,host=" + socket.gethostname() + ",interface=" + interface + " " + json_interface_data[interface]["linkstate"])
-
-
-if __name__ == "__main__":
-
-    interface_data()
-
-    exit(0)
+exit(0)
