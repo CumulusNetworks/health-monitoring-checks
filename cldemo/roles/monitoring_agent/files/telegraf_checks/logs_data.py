09be9f81ca15d0583884f1077b301a27b3d5908d
rdarbha <rama.h.darbha@gmail.com>
2016-09-15 11:37:50 -0400

    Adding script to upload logs as metric

--- cldemo/roles/monitoring_agent/files/telegraf_checks/logs_data.py
+++ cldemo/roles/monitoring_agent/files/telegraf_checks/logs_data.py
@@ -1,8 +1,22 @@
 from pygtail import Pygtail
 import sys
+from output_module import ExportData
 
-for line in Pygtail("/var/log/syslog"):
-    if "session closed" in line:
-        sys.stdout.write(line)
 
-    # sys.stdout.write(line)
+tmp_output = []
+tmp_line = ""
+
+
+
+def parse_logs():
+    data = ExportData("logs")
+    for line in Pygtail("/var/log/syslog"):
+        if "ADJCHANGE" in line and "Down BGP Notification" in line:
+            data.add_row = [{"msg":"log"},{"peer":'"'+str(line.split('')[5])+'"',"reason":"Hold Timer Expired?"}]
+
+        # sys.stdout.write(line)
+    data.send_data("cli")
+
+parse_logs()
+
+exit(0)
