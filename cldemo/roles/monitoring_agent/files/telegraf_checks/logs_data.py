315480abc237ccd04d89ec7a438beb6be25dcb1e
rdarbha <rama.h.darbha@gmail.com>
2016-09-15 13:27:07 -0400

    Adding script to upload logs as metric

--- cldemo/roles/monitoring_agent/files/telegraf_checks/logs_data.py
+++ cldemo/roles/monitoring_agent/files/telegraf_checks/logs_data.py
@@ -12,7 +12,7 @@ def parse_logs():
     data = ExportData("logs")
     for line in Pygtail("/var/log/syslog"):
         if "ADJCHANGE" in line and "Down BGP Notification" in line:
-            data.add_row = [{"msg":"log"},{"peer":'"'+str(line.split('')[5])+'"',"reason":"Hold Timer Expired?"}]
+            data.add_row = [{"msg":"log"},{"peer":'"'+str(line.split(' ')[5])+'"',"reason":"Hold Timer Expired?"}]
 
         # sys.stdout.write(line)
     data.send_data("cli")
